228c31a7903c804eadf02432b54c4e564df2af93
Yoli <32923409+YL9@users.noreply.github.com>
2018-05-07 14:50:10 +0800

    Update main.js

--- 非阻塞代码/main.js
+++ 非阻塞代码/main.js
@@ -1,8 +1,8 @@
-var fs = require('fs')
+var fs = require('fs');
 
 fs.readFile('input.txt', function (err, data) {
-  if (err) return console.log(err)
+  if (err) return console.log(err);
   console.log(data.toString());
 });
 
-console.log("程序执行结束")
+console.log("程序执行结束");
